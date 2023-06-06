deadfd8caa56cf913ae8b26174026dbe0f28e240
aalexsd <alexduarteduartesilva@gmail.com>
2023-06-06 18:09:36 -0300

    using hello world to test the application

--- src/main/java/com/example/brfapi/brfapi/BrfapiApplication.java
+++ src/main/java/com/example/brfapi/brfapi/BrfapiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BrfapiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BrfapiApplication.class, args);
 	}
 
--- src/main/java/com/example/brfapi/brfapi/HelloController.java
+++ src/main/java/com/example/brfapi/brfapi/HelloController.java
@@ -1,4 +1,16 @@
 package com.example.brfapi.brfapi;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
+
+    @GetMapping
+    public String olaMundo(){
+        return "Hello Worlddddd";
+    }
+
 }
