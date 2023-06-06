3bd798bbe2ade25725e23771093b6a210441a5c4
aalexsd <alexduarteduartesilva@gmail.com>
2023-06-06 18:38:19 -0300

    changing User Class

--- src/main/java/com/example/brfapi/brfapi/controller/UserController.java
+++ src/main/java/com/example/brfapi/brfapi/controller/UserController.java
@@ -1,15 +1,21 @@
 package com.example.brfapi.brfapi.controller;
 
+import com.example.brfapi.brfapi.user.User;
 import com.example.brfapi.brfapi.user.UserRegisterData;
+import com.example.brfapi.brfapi.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
 
+    @Autowired
+    private UserRepository repository;
+
     @PostMapping
-    public void register(@RequestBody UserRegisterData json){
-        System.out.println(json);
+    public void register(@RequestBody UserRegisterData dados){
+        repository.save(new User(dados));
     }
 
 }
--- src/main/java/com/example/brfapi/brfapi/user/User.java
+++ src/main/java/com/example/brfapi/brfapi/user/User.java
@@ -1,23 +1,29 @@
 package com.example.brfapi.brfapi.user;
 
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode;
 
-@Table(name = "medicos")
+@Table(name = "users")
 @Entity(name = "User")
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor(of = "id")
+@AllArgsConstructor()
 public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String role;
+
+    public User(UserRegisterData dados) {
+        this.name = dados.name();
+        this.role = dados.role();
+    }
 }
